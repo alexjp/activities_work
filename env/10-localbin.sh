a88ebe2fc2e6c7964e2c0552b0f29bfbd6a80d4c
Alexandre Pereira <programming.hubmaking@slmail.me>
2022-12-03 00:56:35 +0000

    "Initial public commit"

--- env/10-localbin.sh
+++ env/10-localbin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #export PATH=$HOME/.local/share/kactivitymanagerd/activities/bin:$HOME/.local/bin:$PATH
-export PATH=$HOME/.config/plasma-workspace/bin:$HOME/.local/bin:$PATH
+export PATH=$HOME/.config/plasma-workspace/bin:$HOME/.local/bin:$HOME/.cargo/bin:$HOME/.npm-packages/bin:$PATH
